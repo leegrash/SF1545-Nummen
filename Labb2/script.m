975d481851743f3262b72b4b1ba44214869257d0
Gustaf Svensson <gustaf.le.svensson@outlook.com>
2021-11-12 14:26:11 +0100

    2.3.2 done

--- Labb2/script.m
+++ Labb2/script.m
@@ -3,7 +3,8 @@ function script()
     %plotPart2([0, 0.5, 1, 1.5, 2, 2.99, 3], [0, 0.52, 1.09, 1.75, 2.45, 3.5, 4]);
     
     %Del 3
-    part3LS()
+    %part3fun5LS([150, 200, 300, 500, 1000, 2000], [2, 3, 4, 5, 6, 7]);
+    part3fun6LS([150, 200, 300, 500, 1000, 2000], [2, 3, 4, 5, 6, 7]);
 end
 
 %Del 2:
@@ -54,13 +55,10 @@ function plotPart2(xList, yList)
 end
 
 %Del 3:
-function part3LS()
-    alpha = [150, 200, 300, 500, 1000, 2000];
-    U = [2, 3, 4, 5, 6, 7];
-    
+function part3fun5LS(alpha, u)
     aFunc = @(x, y) x./y - x./8; 
     
-    sysR = aFunc(alpha, U)';
+    sysR = aFunc(alpha, u)';
     
     sysL = ones(6, 1);
     
@@ -74,4 +72,35 @@ function part3LS()
     
     plot(graphRangeX, graphRangeY);
     
+end
+
+function part3fun6LS(alpha, u)
+    LSLeft = [
+        1 log(alpha(1)); 
+        1 log(alpha(2)); 
+        1 log(alpha(3)); 
+        1 log(alpha(4));
+        1 log(alpha(5)); 
+        1 log(alpha(6))
+        ];
+    LSRight = [log(8-u(1)); log(8-u(2)); log(8-u(3)); log(8-u(4)); log(8-u(5)); log(8-u(6))];
+    
+    koeff = LSLeft\LSRight;
+    
+    a = exp(koeff(1));
+    b = koeff(2);
+    
+    UFunc = @(x) 8 - a.*x.^b;
+    
+    graphRangeX = 100:1:3000;
+    
+    graphRangeY = UFunc(graphRangeX);
+    
+    plot(alpha, u, ".");
+    hold on
+    
+    xlim([100, 3000]);
+    ylim([0, 9]);
+    
+    plot(graphRangeX, graphRangeY);
 end
